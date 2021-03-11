7647403621e2ae64e4810c015f10392c583ba6b4
ValdirCezar <valdircezar312@gmail.com>
2021-03-11 09:16:30 -0300

    Tratamento de exceções para objeto não encontrado

---
+++ senai-vacinas/src/main/java/com/valdir/senaivacinas/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.valdir.senaivacinas.resources.exceptions;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.valdir.senaivacinas.services.exceptions.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e, HttpServletRequest request) {
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(), e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+}
--- senai-vacinas/src/main/java/com/valdir/senaivacinas/services/PaisService.java
+++ senai-vacinas/src/main/java/com/valdir/senaivacinas/services/PaisService.java
@@ -8,18 +8,20 @@ import org.springframework.stereotype.Service;
 
 import com.valdir.senaivacinas.domain.Pais;
 import com.valdir.senaivacinas.repositories.PaisRepository;
+import com.valdir.senaivacinas.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class PaisService {
 
 	@Autowired
 	private PaisRepository repository;
-	
+
 	public Pais findById(Integer id) {
 		Optional<Pais> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não contrado! Id: " + id + ", Tipo: " + Pais.class.getName()));
 	}
-	
+
 	public List<Pais> findAll() {
 		return repository.findAll();
 	}
