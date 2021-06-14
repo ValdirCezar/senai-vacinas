852aed5893b620128b4a81907e3d95ae13670fb7
Valdir Cezar <valdircezar312@gmail.com>
2021-06-14 14:42:06 -0300

    liberando filtro de cors

--- senai-vacinas/src/main/java/com/valdir/senaivacinas/configurations/SecurityConfig.java
+++ senai-vacinas/src/main/java/com/valdir/senaivacinas/configurations/SecurityConfig.java
@@ -38,12 +38,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
     private JWTUtil jwtUtil;
 	
-	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**/", "/login/**" };
+	private static final String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 	private static final String[] PUBLIC_MATCHERS_POST = { "/usuarios/**"};
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
+		http.cors();
 		
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
